73751dcdc95fadd87342eadc1ed17aa202fe2e58
Patryk Sosiński2 <patryk.sosinski2@assecods.pl>
2022-01-13 18:35:53 +0100

    Initial commit

---
+++ src/test/java/it/sosinski/financecontrol/FinanceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sosinski.financecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
