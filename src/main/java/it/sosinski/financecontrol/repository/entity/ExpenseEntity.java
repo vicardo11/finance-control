5a4b0b51d0f5a1b2f53ab58f2388c0bd90a594c9
Patryk Sosiński2 <patryk.sosinski2@assecods.pl>
2022-01-20 21:01:16 +0100

    ExpenseEntity refactor

--- src/main/java/it/sosinski/financecontrol/repository/entity/ExpenseEntity.java
+++ src/main/java/it/sosinski/financecontrol/repository/entity/ExpenseEntity.java
@@ -1,11 +1,18 @@
 package it.sosinski.financecontrol.repository.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
 
 import static it.sosinski.financecontrol.repository.entity.EntityConstants.TABLE_NAME_EXPENSES;
 
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name = TABLE_NAME_EXPENSES)
 public class ExpenseEntity {
@@ -20,45 +27,7 @@ public class ExpenseEntity {
 
     private BigDecimal price;
 
-    public Long getExpenseId() {
-        return expenseId;
-    }
+    @ManyToOne
+    private ExpenseCategoryEntity expenseCategoryEntity;
 
-    public void setExpenseId(Long expenseId) {
-        this.expenseId = expenseId;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "ExpenseEntity{" +
-                "expenseId=" + expenseId +
-                ", date=" + date +
-                ", categoryId=" + categoryId +
-                ", price=" + price +
-                '}';
-    }
 }
