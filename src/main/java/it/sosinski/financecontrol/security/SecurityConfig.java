8e72731b0a06af30a18343a9043d6ef08434c710
Patryk Sosiński2 <patryk.sosinski2@assecods.pl>
2022-01-29 09:20:41 +0100

    Spring Security - authorize requests (permit all only to register)

--- src/main/java/it/sosinski/financecontrol/security/SecurityConfig.java
+++ src/main/java/it/sosinski/financecontrol/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package it.sosinski.financecontrol.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -32,6 +34,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable();
+                .csrf().disable()
+                .authorizeRequests().antMatchers("/ACCOUNTS/REGISTRATION").permitAll()
+                .and()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic();
     }
 }
