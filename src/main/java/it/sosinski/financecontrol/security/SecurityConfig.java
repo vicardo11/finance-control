209efdf061753151a4afc537a2d74bb202fe2319
Patryk Sosiński <patryk.sosinski1@gmail.com>
2022-06-30 20:11:16 +0200

    Add profiles to SecurityConfig

--- src/main/java/it/sosinski/financecontrol/security/SecurityConfig.java
+++ src/main/java/it/sosinski/financecontrol/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package it.sosinski.financecontrol.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@Profile(value = {"development", "production"})
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final FinanceControlUserDetailsService financeControlUserDetailsService;
